82793010191e144491b48836c3966610d1db4070
TIM-Coder07 <sajonwdd72566@gmail.com>
2026-05-03 16:12:00 +0600

    Fixed dplm issues

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,6 +3,7 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.DB_URI);
+console.log(process.env.DB_URI);
 const db = client.db("tiles-db");
 
 export const auth = betterAuth({
