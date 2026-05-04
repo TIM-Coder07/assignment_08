36bf0fb538b54e940a6348fa00631f712b1d5de4
TIM-Coder07 <sajonwdd72566@gmail.com>
2026-05-04 17:43:04 +0600

    Fixed the loadding issue

--- src/app/(main)/products/page.jsx
+++ src/app/(main)/products/page.jsx
@@ -1,20 +1,9 @@
-"use client";
-
 import ProductCard from "@/Component/ProductsCard";
-import { useEffect, useState } from "react";
 
-const ProductsPage = () => {
-  const [data, setData] = useState([]);
+const ProductsPage = async () => {
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("https://assignment-08-fake-server.onrender.com/products");
-      const json = await res.json();
-      setData(json);
-    };
-
-    fetchData();
-  }, []);
+  const res = await fetch("https://assignment-08-fake-server.onrender.com/products");
+  const data = await res.json();
 
   return (
     <div className="container mx-auto p-6 grid grid-cols-1 md:grid-cols-3 gap-6">
